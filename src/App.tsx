a6c23e786c0465799581851f4c1105d8caaf14a2
EiLuca <luis1401cano2005@gmail.com>
2025-04-19 16:46:38 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
 import './App.css'
 import ButtonSecondary from './components/ButtonSecondary'
 import saladLogo from './assets/salad-logo.jpg'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="container">
-      <div className="logo">
-        <img src={saladLogo} alt="logo" />
-      </div>
-      <div className="title">
-        <span>Fast Delivery at your doorstep</span>
-      </div>
-      <div className="subtitle">
-        <span>Home delivery and online reservations system for restaurants & cafe</span>
-      </div>
-      <ButtonSecondary text="let's Explore" link="./components/Home" />
-    </div>
+    <Routes>
+      <Route path="/" element={
+        <div className="container">
+          <div className="logo">
+            <img src={saladLogo} alt="logo" />
+          </div>
+          <div className="title">
+            <span>Fast Delivery at your doorstep</span>
+          </div>
+          <div className="subtitle">
+            <span>Home delivery and online reservations system for restaurants & cafe</span>
+          </div>
+          <ButtonSecondary text="let's Explore" link="/home" />
+        </div>
+      } />
+      <Route path="/home" element={<Home />} />
+    </Routes>
   )
 }
 
